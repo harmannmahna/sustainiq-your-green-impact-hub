6f451f9fe6829818cf3bd7585c16d6e8f19d36e2
harmann <harmann3883@gmail.com>
2026-06-22 03:25:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
-      deployment: {
-        preset: 'netlify'
-      }
-    }),
+    tanstackStart(),
     tsconfigPaths()
   ],
 })
