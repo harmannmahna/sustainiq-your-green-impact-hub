875c16dfb5afaafc35e642ef182e6d3b7fe9dfb5
harmann <harmann3883@gmail.com>
2026-06-22 03:10:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
-    viteReact(),
-    tsConfigPaths()
+    tsconfigPaths()
   ],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
 })
