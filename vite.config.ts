4f2e0c4af310ca2736b758aaad5fac40f754c318
harmann <harmann3883@gmail.com>
2026-06-22 03:21:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
+  plugins: [
+    tanstackStart({
+      deployment: {
+        preset: 'netlify'
+      }
+    }),
+    tsconfigPaths()
+  ],
 })
